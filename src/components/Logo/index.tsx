0df6b32a6037d32d55e4ee33eae78c50f38dc947
Eduardo Ribeiro <eduardor.contato@outlook.com>
2022-10-13 11:34:30 -0300

    Fixed logo src

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as S from './styles'
 
-const Logo = () => <S.LogoWrapper src="/img/logoOncretsAzulePreto.png" />
+const Logo = () => <S.LogoWrapper src="/img/LogoOncretsAzulePreto.png" />
 
 export default Logo
